b8b9be279d877fc3f6b263e5b70bcbb5661bef83
Sergio Garcia Prado <sergio@garciparedes.me>
2017-10-01 14:35:51 +0200

    Started to work on Simple Random Sampling practices

---
+++ statistical-sampling/simple-random-sampling/exercise-01.m
@@ -0,0 +1,25 @@
+%{
+
+  University: Universidad de Valladolid
+  Degree: Grado en Estadística
+  Subject: Muestreo Estadístico 1
+  Year: 2017/18
+  Teacher: Jesús Alberto Tapia García
+  Author: Sergio García Prado (garciparedes.me)
+  Name: simple-random-sampling/exercise-01.m
+
+%}
+
+%{
+  Col 1: Myoinositol
+  Col 2: Scylloinositol
+  Col 3: D:HI
+  Col 4: D:HII
+%}
+DATA = load('../../data/adulvino.mat').datos;
+DATA = DATA(2:size(DATA,1),:);
+
+
+s_piloto = DATA(unifrnd(0, 1, size(DATA,1), 1) < 0.1,1);
+size(s_piloto,1)
+size(s_piloto(s_piloto >= 1525,1),1)
