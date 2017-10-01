2b860be0b2c78269b1c4ac3ca872d508f39c016a
Sergio Garcia Prado <sergio@garciparedes.me>
2017-10-01 13:06:58 +0200

    Done with first practice

--- statistical-sampling/bernoulli/bernoulli-01.m
+++ statistical-sampling/bernoulli/bernoulli-01.m
@@ -19,7 +19,13 @@ N = 600;
 
 % Simulemos los datos poblacionales para poder tomar la muestra
 datpob = unifrnd(0, 10, N, 1);
+mu = mean(datpob);
 pi_k = 1 / 6;
+
+% Valor crítico para z_0.975
+alpha_value = 0.05;
+z = norminv(1 - alpha_value / 2, 0, 1);
+
 % Considerara dos contadores
 contpi = 0;
 contalt = 0;
@@ -28,62 +34,68 @@ contalt = 0;
 % dos metodos nit=100 veces y comprobar cuantas veces los ICs contienen al
 % verdadero valor del parametro con objeto de confirmar que los ICs
 % obtenidos tienen la confianza deseada (95%)
+nit = 100;
+for h = 1:nit
 
-% Tomemos la muestra
+  % Tomemos la muestra
 
-s = datpob(unifrnd(0, 1, N, 1) < pi_k);
-
-
-% Utilizar una funcion para tomar la muestra
-% individuos que conforman la muestra
-I = selecmuesbernll(N, pi_k);
-% Tomemos las respuestas de los individuos seleccionados
-s = datpob(I);
-
-% Estimemos la nota media poblacional utilizandio las dos alternativas de
-% estimadores
-
-% piestimador de la nota total de los 600 alumnos
-pi_estimador = sum(s)/pi_k;
-
-% Estimacion del ECM e la nota total de los 600 alumnos
-
-var_pi_estimador = (1 / pi_k) * (1 / pi_k-1) * sum(s .^ 2);
-
-% Error de muestreo de la media poblacional
-error_muestreo_pi_estimador = sqrt(var_pi_estimador);
-
-% Error de estimacion fijada una confianza del 95%
-
-alpha_value = 0.05;
-
-% IC del 95% para mu es
-IC_range = norminv(1-alpha_value / 2, 0, 1) * error_muestreo_pi_estimador;
-IC_izq = pi_estimador - IC_range;
-IC_der = pi_estimador + IC_range;
-[IC_izq, IC_der]
-pi_estimador
-sum(datpob)
-% Comprobamos si el IC contiene al parámetro media poblacional
-IC_izq < sum(datpob) && sum(datpob) < IC_der
+  % s = datpob(unifrnd(0, 1, N, 1) < pi_k);
 
 
-%Estimar el parametro total poblacional con el estimador alternativo
-%Tamaño de la muestra seleccionada
+  % Utilizar una funcion para tomar la muestra
+  % individuos que conforman la muestra
+  I = selecmuesbernll(N, pi_k);
+  % Tomemos las respuestas de los individuos seleccionados
+  s = datpob(I);
+  Ns = size(s, 1);
+  % Estimemos la nota media poblacional utilizandio las dos alternativas de
+  % estimadores
 
-%La formula 11 depende de la cuasivarianza POBLACIONAL
-%Tengo que estimar la cuasivarianza poblacional y propongo estimarla con la
-%cusivarianza muetral
-%ecm taotal est alternativo
+  % piestimador de la nota total de los 600 alumnos
+  pi_estimador = sum(s)/pi_k;
 
-%Estimemso la media con estimador alternativo
+  % Estimacion del ECM de la nota total de los 600 alumnos
 
-%Error de estimacion fijada una confianza del 95%
+  var_pi_estimador = (1 / pi_k) * (1 / pi_k-1) * sum(s .^ 2);
 
-%IC del 95% para la media poblacional es
+  media_pi_estimador = pi_estimador / N
 
+  % Error de muestreo de la media poblacional
+  % Error de estimacion fijada una confianza del 95%
+  % IC del 95% para mu es
+  IC_range =  z * sqrt(var_pi_estimador / N^2);
+  IC_izq = media_pi_estimador - IC_range;
+  IC_der = media_pi_estimador + IC_range;
 
-%Confioanza de los intervalos
+  % Comprobamos si el IC contiene al parámetro media poblacional
+  contpi = contpi + (IC_izq < mu && mu < IC_der);
+
+  %Estimar el parametro total poblacional con el estimador alternativo
+  %Tamaño de la muestra seleccionada
+
+  alt_estimador = N / Ns * sum(s);
+
+  % La formula 11 depende de la cuasivarianza POBLACIONAL
+  % Tengo que estimar la cuasivarianza poblacional y propongo estimarla con la
+  % cuasivarianza muestral ecm total del estimador alternativo
+  var_alt_estimador = N * (1 / pi_k - 1) * var(s);
+
+  %Estimación de la media con estimador alternativo
+  media_alt_estimador = alt_estimador / N
+
+  %Error de estimacion fijada una confianza del 95%
+  IC_range =  z * sqrt(var_alt_estimador / N^2);
+
+  %IC del 95% para la media poblacional es
+  IC_izq = media_alt_estimador - IC_range;
+  IC_der = media_alt_estimador + IC_range;
+  %Confianza de los intervalos
+  contalt = contalt + (IC_izq < mu && mu < IC_der);
+
+endfor
+
 %Metodo piestimador
+ratio_pi = contpi / nit
 
-%Metodo alterbnativo
+%Metodo alternativo
+ratio_alt = contalt / nit
