359c1d33b1735e5b183b333e9843177a58e42c0a
Sergio Garcia Prado <sergio@garciparedes.me>
2017-10-09 12:05:22 +0200

    Done with exam of 15/16

--- statistical-sampling/simple-random-sampling/exam-15-16.m
+++ statistical-sampling/simple-random-sampling/exam-15-16.m
@@ -19,6 +19,9 @@ N1 = size(U1,1)
 U2 = [DATA.numemp, DATA.empsatcont];
 N2 = size(U2,1)
 
+alpha = 0.95
+confianza = 1 - (1 - alpha) / 2
+k = norminv(confianza, 0,1)
 
 %{
   Objetivo 1: Considerando m.a.s. sin reemplazamiento
@@ -31,16 +34,26 @@ N2 = size(U2,1)
     Estimar la satisfacción media con el seguro de los asegurados privados.
 %}
 
-alpha = 0.95
-confianza = 1 - (1 - alpha / 2)
+
 
 n1 = calcnPmas(N1, confianza, 0.051)
 f1 = n1 / N1;
 i1 = mas(N1, n1);
 s1 = U1(i1,:);
 
+s1_est_var_2 = var(s1(:,2)) * n1 /(n1-1);
+
 p1_est = sum(s1(:,1)) / n1
+p1_est_var = (1 - f1) / (n1 - 1) * p1_est * (1 - p1_est)
+p1_est_bound = k * sqrt(p1_est_var);
+p1_est_ic = [p1_est - p1_est_bound, p1_est + p1_est_bound]
+
+
 mu1_est = sum(s1(:,2)) / n1
+mu1_est_var = (1-f1)/(n1) * s1_est_var_2;
+mu1_est_bound = k * sqrt(mu1_est_var);
+mu1_est_ic = [mu1_est - mu1_est_bound, mu1_est + mu1_est_bound]
+
 
 %{
   Objetivo 2: Considerando m.a.s. con reemplazamiento
@@ -55,3 +68,33 @@ mu1_est = sum(s1(:,2)) / n1
     Estimar el número total de asegurados en empresa.
     Estimar el total de asegurados en empresa satisfechos con la aseguradora
 %}
+
+n2_piloto = 10
+i2_piloto = mas(N2, n2_piloto);
+s2_piloto = U2(i2_piloto, :);
+
+
+s2_piloto_est_var_2 = var(s2_piloto(:,2)) * n2_piloto /(n2_piloto-1);
+
+
+n2 = ceil((N2 ^ 2 * k ^ 2 * s2_piloto_est_var_2)/(1400 ^ 2))
+f2 = n2/N2;
+
+i2 = mascon(N2, n2);
+s2 = U2(i2, :);
+s2_est_var_1 = var(s2(:,1)) * n2 /(n2-1);
+s2_est_var_2 = var(s2(:,2)) * n2 /(n2-1);
+
+tau2_est = sum(s2(:,1)) / f2
+tau2_est_var = N2 ^ 2 / n2 * s2_est_var_1
+tau2_est_bound = k * sqrt(tau2_est_var)
+tau2_est_ic = [tau2_est - tau2_est_bound, tau2_est + tau2_est_bound]
+
+
+tau3_est = sum(s2(:,2)) / f2
+tau3_est_var = N2 ^ 2 / n2 * s2_est_var_2
+tau3_est_bound = k * sqrt(tau3_est_var)
+tau3_est_ic = [tau3_est - tau3_est_bound, tau3_est + tau3_est_bound]
+
+
+p2_est = tau3_est / tau2_est
