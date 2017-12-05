a10a1c5fe374f16a39b85c889b6c73d66fe37e9c
Sergio Garcia Prado <sergio@garciparedes.me>
2017-12-05 20:11:30 +0100

    Created Cluster Sampling exercise 1 file

---
+++ statistical-sampling/cluster-sampling/exercise-01.m
@@ -0,0 +1,14 @@
+%{
+
+  Universits: Universidad de Valladolid
+  Degree: Grado en Estadística
+  Subject: Muestreo Estadístico 1
+  year: 2017/18
+  Teacher: Jesús Alberto Tapia García
+  Author: Sergio García Prado (garciparedes.me)
+  Name: cluster-sampling/exercise-01.m
+
+%}
+clear;
+
+addpath(genpath('./../utils/'));
